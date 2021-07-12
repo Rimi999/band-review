41026e8ac84dcbd04dd2efb0152f9396b78d6527
Rimi999 <all_wom@naver.com>
2021-07-12 23:32:24 +0900

    지도 넣기

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ function init() {
 	mNavi()
 	slide()
 	modal()
+	map()
 }
 
 function navi() {
@@ -59,4 +60,14 @@ function modal() {
 	$('.modal-wrapper .modal-wrap').click(function(e) {
 		e.stopPropagation()
 	})
+}
+
+function map() {
+	var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+	var options = { //지도를 생성할 때 필요한 기본 옵션
+		center: new kakao.maps.LatLng(37.55524094809751, 126.9369011148819), //지도의 중심좌표.
+		level: 3 //지도의 레벨(확대, 축소 정도)
+	};
+
+var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
 }
